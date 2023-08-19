4a209660f18e9840c13925aa4279333d0f9ac206
Gireada Ovidiu <80890138+OvidiuGi@users.noreply.github.com>
2023-08-19 03:58:49 +0300

    [API-4] Added github CI for phpcs, phpunit and phpstan (#4)
    * [API-4] Added github CI for phpcs, phpunit and phpstan
    
    * [API-4] Changed database credentials
    
    * [API-4] Created user for database and installed phpunit
    
    * [API-4] Fixed qulity control problems
    
    * [API-4] Added phpcs.xml.dist
    
    * [API-4] Deleted phpcs.xml.dist
    
    * Feature/api 5 - Started Implementing Tests (#5)
    
    * [API-5] Created fixtures and one useless test
    
    * [API-5] Updated command for pipeline
    
    ---------
    
    Co-authored-by: Ovidiu Gireada <ovidiugireada@192-168-0-101.rdsnet.ro>
    
    ---------
    
    Co-authored-by: Ovidiu Gireada <ovidiugireada@192-168-0-101.rdsnet.ro>

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\AverageFinishTimeService;
+use App\Entity\Race;
+use App\Entity\Result;
+use App\ResultCalculationService;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\DBAL\Exception;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private readonly AverageFinishTimeService $averageFinishTimeService,
+        private readonly ResultCalculationService $resultCalculationService
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i <= 10; $i++) {
+            $race = new Race();
+            $race->title = 'Race ' . $i;
+            $race->setDate(new \DateTimeImmutable());
+
+            for ($j = 0; $j <= 10; $j++) {
+                $result = new Result();
+                $result->fullName = 'Runner ' . $j . ' Race ' . $i;
+                $result->distance = $j % 2 === 0 ? Result::DISTANCE_MEDIUM : Result::DISTANCE_LONG;
+                $result->ageCategory = $j % 2 === 0 ? 'M20-25' : 'F20-25';
+                $result->setFinishTime(new \DateTimeImmutable("+{$j} minutes"));
+                $result->setRace($race);
+
+                $manager->persist($result);
+            }
+
+            $manager->persist($race);
+            $manager->flush();
+
+            $this->setPlacementsAndAverageFinishTime($race);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function setPlacementsAndAverageFinishTime(Race $race): void
+    {
+        $this->resultCalculationService->updatePlacementsForResults($race);
+        $this->resultCalculationService->updateAgeCategoryPlacementsForResults($race);
+        $this->averageFinishTimeService->updateAverageFinishTimeForMediumAndLongRaces($race);
+    }
+}
